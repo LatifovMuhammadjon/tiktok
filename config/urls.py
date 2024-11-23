974711e35f62de367d1d6a7e0a4ac46d29d1feef
LatifovMuhammadjon <latifov7864@gmail.com>
2024-11-23 16:09:13 +0500

    updated

--- config/urls.py
+++ config/urls.py
@@ -5,5 +5,5 @@ from tiktok_auth.views import file_download_view
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('tiktok/', include('tiktok_auth.urls')),
-    path('', file_download_view)
+    path('tiktokwky1RKcvmi5COrMgUDl7AzhZ4MiVjfva.txt', file_download_view)
 ]
